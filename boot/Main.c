fef77b7eac4f3959bda1e4dcdb8750fa043c23df
Marco4680 <maverick9xd@outlook.com>
2024-08-15 07:36:07 -0400

    feat: uart get char

--- boot/Main.c
+++ boot/Main.c
@@ -17,6 +17,14 @@ void main(void)
 
     putstr("Hello World!\n");
 
+    i = 100;
+    while (i--)
+    {
+        uint8_t ch = Hal_uart_get_char();
+        Hal_uart_put_char(ch);
+    }
+    
+
 }
 
 static void Hw_init(void)
